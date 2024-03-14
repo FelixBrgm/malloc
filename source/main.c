e76dab3e926434c1c18188cea80e743538469e7e
FelixBrgm <github.badly321@passinbox.com>
2024-03-14 16:00:17 +0000

    Fixed linking (tf was this fix why it working!?!?)

--- source/main.c
+++ source/main.c
@@ -3,20 +3,11 @@ int main()
 {
     for (size_t i = 0; i < 200 * 100; i++)
     {
-        void *test = ft_malloc(10);
-        ft_free(test);
-        printf("POINTER 1: %p\n", test);
-        test = ft_malloc(10);
-        printf("POINTER 2: %p\n", test);
+        void *test = malloc(10);
+        free(test);
+        test = malloc(10);
     }
 
     printf("Capacity %i\n", storage.capacity);
-
-    uint8_t asd = 0;
-    activate_bit(&asd, 6);
-
-    printf("VALUE: %zu\n", get_bit(asd, 7));
-    printf("VALUE: %zu\n", get_bit(asd, 6));
-
     return (0);
 }
