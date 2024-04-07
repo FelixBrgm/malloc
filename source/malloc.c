ea17732ef52835250c064ded4c3dc8bab8c04893
Felix Brüggemann <gateway_painter_03@icloud.com>
2024-04-07 14:50:15 +0000

    Changed to .so working solution

--- source/malloc.c
+++ source/malloc.c
@@ -10,9 +10,8 @@ t_storage storage = {
 void *get_block_memory(size_t size, uint8_t block_type);
 void *get_single_memory(size_t size);
 
-void *ft_malloc(size_t size)
+void *malloc(size_t size)
 {
-    printf("HELLO\n");
     if (storage.page_size == 0)
         storage.page_size = getpagesize();
 
