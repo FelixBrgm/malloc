a224a739266d892bacef08f8a642f2a047232c5c
FelixBrgm <github.badly321@passinbox.com>
2024-09-27 16:48:27 +0000

    FIX: only reallocate single block if bigger size requested && only memove prev_size

--- source/realloc.c
+++ source/realloc.c
@@ -2,6 +2,23 @@
 
 t_zone *get_zone(uint8_t *ptr);
 
+static uint32_t get_size_of_zone(t_zone *zone)
+{
+    uint32_t previous_size = 0;
+
+    if (zone->type == ZONE_TYPE_BLOCK)
+    {
+        t_metadata_block metadata = read_metadata_block_from_array(zone->mem);
+        previous_size = metadata.size_of_each_block;
+    }
+    else if (zone->type == ZONE_TYPE_SINGLE)
+    {
+        previous_size = zone->size;
+    }
+
+    return (previous_size);
+}
+
 void *realloc(void *ptr, size_t size)
 {
     if (ptr == NULL)
@@ -11,17 +28,15 @@ void *realloc(void *ptr, size_t size)
     if (zone == NULL)
         return (malloc(size));
 
-    if (zone->type == ZONE_TYPE_BLOCK)
-    {
-        t_metadata_block metadata = read_metadata_block_from_array(zone->mem);
-        if (metadata.size_of_each_block >= size)
-            return (ptr);
-    }
+    uint32_t previous_size = get_size_of_zonej(zone);
+
+    if (previous_size >= size)
+        return (ptr);
 
     uint8_t *new_ptr = malloc(size);
     if (new_ptr == NULL)
         return (NULL);
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < previous_size; i++)
     {
         new_ptr[i] = ((uint8_t *)ptr)[i];
     }
